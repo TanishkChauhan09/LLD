52fd57c6b71ec1765d253ee704ca5b2b6e23145a
TANISHK CHAUHAN <iamtanishkchauhan@gmail.com>
2025-07-14 21:08:45 +0530

    Walk and talk

---
+++ StrategyDesignPattern.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+// --- Strategy Interface for Walk ---
+class WalkableRobot {
+public:
+    virtual void walk() = 0;
+    virtual ~WalkableRobot() {}
+};
+
+// --- Concrete Strategies for walk ---
+class NormalWalk : public WalkableRobot {
+public:
+    void walk() override { 
+        cout << "Walking normally..." << endl; 
+    }
+};
+
+class NoWalk : public WalkableRobot {
+public:
+    void walk() override { 
+        cout << "Cannot walk." << endl; 
+    }
+};
+
+
+// --- Strategy Interface for Talk ---
+class TalkableRobot {
+public:
+    virtual void talk() = 0;
+    virtual ~TalkableRobot() {}
+};
+
+// --- Concrete Strategies for Talk ---
+class NormalTalk : public TalkableRobot {
+public:
+    void talk() override { 
+        cout << "Talking normally..." << endl; 
+    }
+};
+
+class NoTalk : public TalkableRobot {
+public:
+    void talk() override { 
+        cout << "Cannot talk." << endl; 
+    }
+};
