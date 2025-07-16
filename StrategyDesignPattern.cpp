1290a3e452be22b783f40f40c338660cb737dc53
TANISHK CHAUHAN <iamtanishkchauhan@gmail.com>
2025-07-16 18:39:14 +0530

    completed strategy

--- StrategyDesignPattern.cpp
+++ StrategyDesignPattern.cpp
@@ -45,16 +45,40 @@ public:
         cout << "Cannot talk." << endl; 
     }
 };
+
+// --- Strategy Interface for Fly ---
+class FlyableRobot {
+public:
+    virtual void fly() = 0;
+    virtual ~FlyableRobot() {}
+};
+
+class NormalFly : public FlyableRobot {
+public:
+    void fly() override { 
+        cout << "Flying normally..." << endl; 
+    }
+};
+
+class NoFly : public FlyableRobot {
+public:
+    void fly() override { 
+        cout << "Cannot fly." << endl; 
+    }
+};
+
 // --- Robot Base Class ---
 class Robot {
 protected:
     WalkableRobot* walkBehavior;
     TalkableRobot* talkBehavior;
+    FlyableRobot* flyBehavior;
 
 public:
-    Robot(WalkableRobot* w, TalkableRobot* t) {
+    Robot(WalkableRobot* w, TalkableRobot* t, FlyableRobot* f) {
         this->walkBehavior = w;
         this->talkBehavior = t;
+        this->flyBehavior = f;
     }
        
     void walk() { 
@@ -63,6 +87,49 @@ public:
     void talk() { 
         talkBehavior->talk(); 
     }
+    void fly() { 
+        flyBehavior->fly(); 
+    }
 
     virtual void projection() = 0; // Abstract method for subclasses
 };
+
+// --- Concrete Robot Types ---
+class CompanionRobot : public Robot {
+public:
+    CompanionRobot(WalkableRobot* w, TalkableRobot* t, FlyableRobot* f)
+        : Robot(w, t, f) {}
+
+    void projection() override {
+        cout << "Displaying friendly companion features..." << endl;
+    }
+};
+
+class WorkerRobot : public Robot {
+public:
+    WorkerRobot(WalkableRobot* w, TalkableRobot* t, FlyableRobot* f)
+        : Robot(w, t, f) {}
+
+    void projection() override {
+        cout << "Displaying worker efficiency stats..." << endl;
+    }
+};
+
+// --- Main Function ---
+int main() {
+    Robot *robot1 = new CompanionRobot(new NormalWalk(), new NormalTalk(), new NoFly());
+    robot1->walk();
+    robot1->talk();
+    robot1->fly();
+    robot1->projection();
+
+    cout << "--------------------" << endl;
+
+    Robot *robot2 = new WorkerRobot(new NoWalk(), new NoTalk(), new NormalFly());
+    robot2->walk();
+    robot2->talk();
+    robot2->fly();
+    robot2->projection();
+
+    return 0;
+}
