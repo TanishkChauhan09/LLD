53560ccdb6ac73dadb0d88a5c4c2f22044650ab3
TANISHK CHAUHAN <iamtanishkchauhan@gmail.com>
2025-08-15 21:51:59 +0530

    Notification system

--- Notificationsystem.cpp
+++ Notificationsystem.cpp
@@ -64,3 +64,84 @@ public:
         return notification->getContent() + "\n-- " + signature + "\n\n";
     }
 };
+
+
+//   Observer Pattern Components
+
+
+// Observer interface: each observer gets an update with a Notification pointer.
+class IObserver {
+public:
+    virtual void update() = 0;
+
+    virtual ~IObserver() {}
+};
+
+class IObservable {
+public:
+    virtual void addObserver(IObserver* observer) = 0;
+    virtual void removeObserver(IObserver* observer) = 0;
+    virtual void notifyObservers() = 0;
+};
+
+// Concrete Observable
+class NotificationObservable :  public IObservable {
+private:
+    vector<IObserver*> observers;
+    INotification* currentNotification;
+public:
+    NotificationObservable() { 
+        currentNotification = nullptr; 
+    }
+
+    void addObserver(IObserver* obs) override {
+        observers.push_back(obs);
+    }
+
+    void removeObserver(IObserver* obs) override {
+        observers.erase(remove(observers.begin(), observers.end(), obs), observers.end());
+    }
+
+    void notifyObservers() override {
+        for (unsigned int i = 0; i < observers.size(); i++) {
+            observers[i]->update();
+        }
+    }
+
+    void setNotification(INotification* notification) {
+        if (currentNotification != nullptr) {
+            delete currentNotification;
+        }
+        currentNotification = notification;
+        notifyObservers();
+    }
+
+    INotification* getNotification() {
+        return currentNotification;
+    }
+
+    string getNotificationContent() {
+        return currentNotification->getContent();
+    }
+
+    ~NotificationObservable() {
+        if (currentNotification != NULL) {
+            delete currentNotification;
+        }
+    }
+};
+    
+// Concrete Observer 1
+class Logger : public IObserver {
+private:
+    NotificationObservable* notificationObservable;
+
+public:
+    Logger(NotificationObservable* observable) {
+        this->notificationObservable = observable;
+    }
+
+    void update() {
+        cout << "Logging New Notification : \n" << notificationObservable->getNotificationContent();
+    }
+};
