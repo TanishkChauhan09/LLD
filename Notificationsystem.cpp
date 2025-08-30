6ced866aaefc0ffab2b870cc821474168a92b5a8
TANISHK CHAUHAN <iamtanishkchauhan@gmail.com>
2025-08-30 22:29:04 +0530

    Notification Service

--- Notificationsystem.cpp
+++ Notificationsystem.cpp
@@ -260,3 +260,36 @@ public:
 };
 
 NotificationService* NotificationService::instance = nullptr;
+
+int main() {
+    // Create NotificationService.
+    NotificationService* notificationService = NotificationService::getInstance();
+
+    // Get Observable
+    NotificationObservable* notificationObservable = notificationService->getObservable();
+   
+    // Create Logger Observer
+    Logger* logger = new Logger(notificationObservable);
+
+    // Create NotificationEngine observers.
+    NotificationEngine* notificationEngine = new NotificationEngine(notificationObservable);
+
+    notificationEngine->addNotificationStrategy(new EmailStrategy("random.person@gmail.com"));
+    notificationEngine->addNotificationStrategy(new SMSStrategy("+91 9876543210"));
+    notificationEngine->addNotificationStrategy(new PopUpStrategy());
+
+    // Attach these observers.
+    notificationObservable->addObserver(logger);
+    notificationObservable->addObserver(notificationEngine);
+
+    // Create a notification with decorators.
+    INotification* notification = new SimpleNotification("Your order has been shipped!");
+    notification = new TimestampDecorator(notification);
+    notification = new SignatureDecorator(notification, "Customer Care");
+    
+    notificationService->sendNotification(notification);
+
+    delete logger;
+    delete notificationEngine;
+    return 0;
+}
