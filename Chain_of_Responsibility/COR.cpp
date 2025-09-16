827ff5705228dd4aff426c1e98f957153c0a7dcc
TANISHK CHAUHAN <iamtanishkchauhan@gmail.com>
2025-09-16 23:49:24 +0530

    Chain of Responsibility used for dependency on previous state

--- Chain_of_Responsibility/COR.cpp
+++ Chain_of_Responsibility/COR.cpp
@@ -17,3 +17,36 @@ public:
 
     virtual void dispense(int amount) = 0;
 };
+
+class ThousandHandler : public MoneyHandler {
+private:
+    int numNotes;
+
+public:
+    ThousandHandler(int numNotes) {
+        this->numNotes = numNotes;
+    }
+
+    void dispense(int amount) override {
+        int notesNeeded = amount / 1000;
+
+        if(notesNeeded > numNotes) {
+            notesNeeded = numNotes;
+            numNotes = 0;
+        }
+        else {
+            numNotes -= notesNeeded;
+        }
+
+        if(notesNeeded > 0)
+            cout << "Dispensing " << notesNeeded << " x ₹1000 notes.\n";
+
+        int remainingAmount = amount - (notesNeeded * 1000);
+        if(remainingAmount > 0) {
+            if(nextHandler != nullptr) nextHandler->dispense(remainingAmount);
+            else {
+                cout << "Remaining amount of " << remainingAmount << " cannot be fulfilled (Insufficinet fund in ATM)\n";
+            }
+        }
+    }
+};
