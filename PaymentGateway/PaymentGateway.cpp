06ca061d606dc4483cdb35f6df81c5bbea3082d8
TANISHK CHAUHAN <iamtanishkchauhan@gmail.com>
2025-09-24 22:24:07 +0530

    Payment Gateway

--- PaymentGateway/PaymentGateway.cpp
+++ PaymentGateway/PaymentGateway.cpp
@@ -31,3 +31,24 @@ public:
     virtual bool processPayment(double amount) = 0;
     virtual ~BankingSystem() {}
 };
+
+class PaytmBankingSystem : public BankingSystem {
+public:
+    PaytmBankingSystem() {}
+    bool processPayment(double amount) override {
+        // Simulate 20% success
+            int r = rand() % 100;
+            return r < 80;
+    }
+};
+
+class RazorpayBankingSystem : public BankingSystem {
+public:
+    RazorpayBankingSystem() {}
+    bool processPayment(double amount) override {
+        cout << "[BankingSystem-Razorpay] Processing payment of " << amount << "...\n";
+        // Simulate 90% success
+        int r = rand() % 100;
+        return r < 90;
+    }
+};
