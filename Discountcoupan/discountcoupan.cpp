548527acf4f5b2740c2e53e1ef6b774d7f4f21eb
TANISHK CHAUHAN <iamtanishkchauhan@gmail.com>
2025-10-07 22:24:12 +0530

    Discount coupan

--- Discountcoupan/discountcoupan.cpp
+++ Discountcoupan/discountcoupan.cpp
@@ -24,3 +24,34 @@ public:
         return min(amount, baseAmount);
     }
 };
+
+
+class PercentageDiscountStrategy : public DiscountStrategy {
+private:
+    double percent;
+public:
+    PercentageDiscountStrategy(double pct) {
+        percent = pct;
+    }
+    double calculate(double baseAmount) override {
+        return (percent / 100.0) * baseAmount;
+    }
+};
+
+class PercentageWithCapStrategy : public DiscountStrategy {
+private:
+    double percent;
+    double cap;
+public:
+    PercentageWithCapStrategy(double pct, double capVal) {
+        percent = pct;
+        cap = capVal;
+    }
+    double calculate(double baseAmount) override {
+        double disc = (percent / 100.0) * baseAmount;
+        if (disc > cap) {
+            return cap;
+        }
+        return disc;
+    }
+};
